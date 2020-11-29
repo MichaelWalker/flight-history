b1efa33422ed7de47071766fd3c8660e5e5ac534
Mike <Walker>
2020-11-29 00:32:14 +0000

    Add a JS client app.

---
+++ FlightHistory/ClientApp/webpack.config.js
@@ -0,0 +1,55 @@
+﻿const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: "./index.js",
+    output: {
+        filename: 'bundle.[fullhash].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+        new webpack.DefinePlugin({
+            USE_SAMPLE_DATA: process.env.USE_SAMPLE_DATA || false,
+        })
+    ],
+    resolve: {
+        modules: [__dirname, 'src', 'node_modules'],
+        extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: 'ts-loader'
+            },
+            {
+                test: /\.module\.scss/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    },
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.scss$/,
+                exclude: /\.module\.scss/,
+                use: ['style-loader','css-loader', 'sass-loader']
+            },
+            {
+                test: /\.png$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+}
--- FlightHistory/Startup.cs
+++ FlightHistory/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +27,9 @@ namespace FlightHistory
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +47,17 @@ namespace FlightHistory
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    ;
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
         }
     }
 }
