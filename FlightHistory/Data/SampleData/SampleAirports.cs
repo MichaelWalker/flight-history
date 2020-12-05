d8e9ec7724b704e5991211e0e6c1b89c0566b097
Mike <Walker>
2020-12-05 02:53:44 +0000

    Add Sample Data when running locally.

---
+++ FlightHistory/Data/SampleData/SampleAirports.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using FlightHistory.Models.Db;
+
+namespace FlightHistory.Data.SampleData
+{
+    public class SampleAirports
+    {
+        public static IEnumerable<Airport> Generate()
+        {
+            return new[]
+            {
+                new Airport
+                {
+                    Name = "Heathrow"
+                },
+                new Airport
+                {
+                    Name = "Gatwick"
+                },
+            };
+        }
+    }
+}
--- FlightHistory/Program.cs
+++ FlightHistory/Program.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Linq;
+using FlightHistory.Data.SampleData;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace FlightHistory
@@ -7,7 +11,38 @@ namespace FlightHistory
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+            var serviceProvider = scope.ServiceProvider;
+            
+            if (IsDevEnvironment(serviceProvider) && DatabaseIsEmpty(serviceProvider))
+            {
+                PopulateWithSampleData(serviceProvider);
+            }
+            
+            host.Run();
+        }
+
+        private static bool IsDevEnvironment(IServiceProvider serviceProvider)
+        {
+            var env = serviceProvider.GetRequiredService<IWebHostEnvironment>();
+            return env.IsDevelopment();
+        }
+        
+        private static bool DatabaseIsEmpty(IServiceProvider serviceProvider)
+        {
+            var db = serviceProvider.GetRequiredService<DatabaseContext>();
+            return !db.Airports.Any();
+        }
+        
+        private static void PopulateWithSampleData(IServiceProvider serviceProvider)
+        {
+            var db = serviceProvider.GetRequiredService<DatabaseContext>();
+            
+            db.Airports.AddRange(SampleAirports.Generate());
+
+            db.SaveChanges();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
