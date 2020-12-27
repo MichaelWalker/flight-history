7f958173d9d81c9276ede11df39bbdf16576c625
Mike <Walker>
2020-12-27 04:18:32 +0000

    Add User Context

--- FlightHistory/ClientApp/src/App.tsx
+++ FlightHistory/ClientApp/src/App.tsx
@@ -5,17 +5,20 @@ import {AirportsPage} from "./pages/Airports";
 import {AircraftPage} from "./pages/Aircraft";
 import {NotFoundPage} from "./pages/NotFound";
 import {DashboardPage} from "./pages/Dashboard";
+import {UserContextProvider} from "./contexts/UserContext";
 
 export const App: FunctionComponent = () => {
     return (
-        <Router>
-            <Switch>
-                <Route path={"/"} exact={true} children={DashboardPage}/>
-                <Route path={"/flights"} exact={true} children={FlightsPage}/>
-                <Route path={"/airports"} exact={true} children={AirportsPage}/>
-                <Route path={"/aircraft"} exact={true} children={AircraftPage}/>
-                <Route path={"/"} children={NotFoundPage}/>
-            </Switch>
-        </Router>
+        <UserContextProvider>
+            <Router>
+                <Switch>
+                    <Route path={"/"} exact={true} children={DashboardPage}/>
+                    <Route path={"/flights"} exact={true} children={FlightsPage}/>
+                    <Route path={"/airports"} exact={true} children={AirportsPage}/>
+                    <Route path={"/aircraft"} exact={true} children={AircraftPage}/>
+                    <Route path={"/"} children={NotFoundPage}/>
+                </Switch>
+            </Router>
+        </UserContextProvider>
     );  
 };
---
+++ FlightHistory/ClientApp/src/contexts/UserContext.tsx
@@ -0,0 +1,41 @@
+﻿import React from "react";
+import {User} from "../models/user";
+import {createContext, FunctionComponent, useEffect, useState} from "react";
+import {accessToken, getCurrentUser} from "../helpers/tokenHelper";
+import {ApiClient} from "../api/apiClient";
+
+interface UserContextProps {
+    loading: boolean;
+    currentUser?: User; 
+}
+
+const UserContext = createContext<UserContextProps>({
+    loading: false,
+    currentUser: undefined,
+})
+
+export const UserContextProvider: FunctionComponent = ({children}) => {
+    const [loading, setLoading] = useState(true);
+    const [currentUser, setCurrentUser] = useState<User | undefined>();
+    
+    function updateUser() {
+        setCurrentUser(getCurrentUser());
+    }
+    
+    useEffect(() => {
+        accessToken.subscribe(updateUser);
+        return () => accessToken.unsubscribe(updateUser);
+    }, []);
+    
+    useEffect(() => {
+        ApiClient.refreshToken()
+            .catch(() => { /* Do nothing - user will be shown the sign in page */ })
+            .finally(() => setLoading(false));
+    });
+    
+    return (
+        <UserContext.Provider value={{ loading, currentUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+}
