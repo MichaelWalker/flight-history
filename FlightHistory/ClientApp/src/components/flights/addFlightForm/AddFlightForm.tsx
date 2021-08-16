0db6983bbd19a8dc434d1435be7322a0731c37f7
Mike Walker <michael.walker@rightmove.co.uk>
2021-08-16 21:07:33 +0100

    Convert to Async Options.

--- FlightHistory/ClientApp/src/components/flights/addFlightForm/AddFlightForm.tsx
+++ FlightHistory/ClientApp/src/components/flights/addFlightForm/AddFlightForm.tsx
@@ -1,8 +1,9 @@
 import type { FC } from "react";
 import React from "react";
+import { Airport } from "../../../models/airport";
 import { stubAirportList } from "../../../models/airport.testdata";
 import { Form } from "../../../wrappers/StyledWrappers";
-import { Select } from "../../forms/select/Select";
+import { Select, SelectOption } from "../../forms/select/Select";
 import { CardSection } from "../../layouts/CardSection";
 import * as styles from "./addFlightForm.styles";
 
@@ -13,11 +14,23 @@ const options = stubAirportList.map((airport) => {
     };
 });
 
+function getOptions(search: string): Promise<SelectOption<Airport>[]> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(
+                options.filter((airport) =>
+                    airport.label.toLowerCase().includes(search.toLowerCase()),
+                ),
+            );
+        }, 500);
+    });
+}
+
 export const AddFlightForm: FC = () => {
     return (
         <CardSection title={"Add Flight"} css={styles.addFlightSection}>
             <Form>
-                <Select options={options} />
+                <Select loadOptions={getOptions} />
             </Form>
         </CardSection>
     );
--- FlightHistory/ClientApp/src/components/forms/select/Select.tsx
+++ FlightHistory/ClientApp/src/components/forms/select/Select.tsx
@@ -1,15 +1,15 @@
 import React, { FC, ReactElement } from "react";
-import ReactSelect from "react-select";
+import AsyncSelect from "react-select/async";
 
-interface Option<T> {
+export interface SelectOption<T> {
     label: string;
     value: T;
 }
 
 interface SelectProps<T> {
-    options: Option<T>[];
+    loadOptions: (search: string) => Promise<SelectOption<T>[]>;
 }
 
-export function Select<T>({ options }: SelectProps<T>): ReactElement {
-    return <ReactSelect options={options} />;
+export function Select<T>({ loadOptions }: SelectProps<T>): ReactElement {
+    return <AsyncSelect loadOptions={loadOptions} />;
 }
